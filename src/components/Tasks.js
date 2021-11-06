d62f4fd266bda4c2305924b5ada04c76b466925e
Matt Mores <79879179+MattMores@users.noreply.github.com>
2021-11-06 17:56:12 -0500

    tasks added

---
+++ src/components/Tasks.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Tasks = () => {
+    const [tasks, setTasks] = useState();
+
+    return (
+        <>
+        {tasks.map((task) => (
+            <h3 key={task.id}>{task.text}</h3>
+        ))}
+        </>
+    )
+}
+
+export default Tasks
